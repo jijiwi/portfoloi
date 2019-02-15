e800126510f65e149b48694eef2825ac30c20e3f
jijiwi <1716357925@qq.com>
2019-02-15 22:17:38 +0800

    添加图片存储功能

--- gallery/models.py
+++ gallery/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 # Create your models here.
-
 # 1、创建model，2、去admin.py注册model
+
+
 class Gallery(models.Model):
-    description = models.CharField(max_length=100)
+    description = models.CharField(default='在此处填写作品简介', max_length=100)
+    image = models.ImageField(default='default.png', upload_to='image/')
+    title = models.CharField(default='标题', max_length=50)
+
+    def __str__(self):
+        return self.title
+
--- portfoloi/settings.py
+++ portfoloi/settings.py
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'  # 媒体文件网址
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # 媒体文件存放路径
